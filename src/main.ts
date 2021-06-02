69ee68ff7ff77c730d2ea46560a7fdfab7e7391b
Matt Riley <matt.riley@metrisk.com>
2021-06-02 15:39:26 +0100

    fix(filepath): throws an error if the manifest file isn't found

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,20 @@ async function cdkStackInfo() {
   try {
     const manifestFilePath = path.join(process.cwd(), "./cdk.out/manifest.json")
     if (fs.existsSync(manifestFilePath)) {
+      core.info(manifestFilePath)
       const { artifacts } = require(manifestFilePath)
       delete artifacts.Tree
       const artifactsKeys = Object.keys(artifacts)
       const stackname = artifactsKeys[0]
       core.setOutput("cdk-stackname", stackname)
+      return
+    } else {
+      throw new Error(`manifest file doesn't exist: ${manifestFilePath}`)
     }
   } catch (err) {
     core.error(err)
     core.setFailed(err)
+    return
   }
 }
 
